11b9e10771ffb3174b777aa36e617f5cdfbd9a52
kdjay517 <kdjay517@gmail.com>
2021-11-21 16:22:31 +0530

    Added: creating a binary sort tree done pre, post and inorder

---
+++ src/Operations.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Operations {
+	Scanner sc = new Scanner(System.in);
+	BinaryTree<Integer> binaryTree = new BinaryTree<>();
+
+	public void preOrderTree() {
+
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		binaryTree.preOrder();
+	}
+
+	public void inOrderTree() {
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		binaryTree.inOrder();
+	}
+
+	public void postOrderTree() {
+
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		binaryTree.postOrder();
+	}
+	
+	public void containsNodeRecursive() {
+		
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		System.out.println(binaryTree.containsNode(67));
+		
+	}
+}
